b69c46bb4a467543cd24e56d9f7e99691b948325
mostafa4460 <mostafa.nassr88@gmail.com>
2023-02-15 16:33:20 -0500

    add navigation to App

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Categories from "./screens/Categories";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
-      <Categories />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={Categories} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
