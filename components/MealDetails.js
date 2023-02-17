6efd166fb17e32afe17d67e415d043945a5cbc70
mostafa4460 <mostafa.nassr88@gmail.com>
2023-02-17 13:50:29 -0500

    add style props to MealDetails component

--- components/MealDetails.js
+++ components/MealDetails.js
@@ -1,11 +1,11 @@
 import { Text, View, StyleSheet } from "react-native";
 
-const MealDetails = ({ duration, complexity, affordability }) => {
+const MealDetails = ({ duration, complexity, affordability, style, textStyle }) => {
   return (
-    <View style={styles.details}>
-      <Text style={styles.detailsItem}>{duration}m</Text>
-      <Text style={styles.detailsItem}>{complexity.toUpperCase()}</Text>
-      <Text style={styles.detailsItem}>{affordability.toUpperCase()}</Text>
+    <View style={[styles.details, style]}>
+      <Text style={[styles.detailsItem, textStyle]}>{duration}m</Text>
+      <Text style={[styles.detailsItem, textStyle]}>{complexity.toUpperCase()}</Text>
+      <Text style={[styles.detailsItem, textStyle]}>{affordability.toUpperCase()}</Text>
     </View>
   );
 };
