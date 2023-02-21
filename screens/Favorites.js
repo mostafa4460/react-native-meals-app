44eda8778a62a88f8bd7cb894203fb59ee6a94ac
mostafa4460 <mostafa.nassr88@gmail.com>
2023-02-20 20:50:17 -0500

    add JSX for Favorites screen

--- screens/Favorites.js
+++ screens/Favorites.js
@@ -1,7 +1,37 @@
-import { Text } from "react-native";
+import { useContext } from "react";
+import { FavoritesContext } from "../store/context/favorites-context";
+import { MEALS } from "../data/dummy-data";
+import MealsList from "../components/MealsList/MealsList";
+import { StyleSheet, View, Text } from "react-native";
 
-const Favorites = () => (
-  <Text>Favorites Screen =)</Text>
+const NoFavorites = () => (
+  <View style={styles.rootContainer}>
+    <Text style={styles.text}>You have no favorite meals yet.</Text>
+  </View>
 );
 
+const Favorites = () => {
+  const { ids } = useContext(FavoritesContext);
+  const favoriteMeals = MEALS.filter((meal) => ids.includes(meal.id));
+
+  return favoriteMeals.length ? (
+    <MealsList data={favoriteMeals} />
+  ) : (
+    <NoFavorites />
+  );
+};
+
 export default Favorites;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
